d6b817dfc8c27508507754c9977f15e3d6d9f486
Sujit Kumar Libi <sujit_libi@hotmail.com>
2026-02-22 14:22:10 +0545

    Fetching all repositories of respective github repo and search filter repo feature implemented

--- module/github/lib/github.ts
+++ module/github/lib/github.ts
@@ -87,3 +87,21 @@ export async function fetchUserContribution(token: string, username: string) {
     return null;
   }
 }
+
+export const getRepositories = async (
+  page: number = 1,
+  perPage: number = 10,
+) => {
+  const token = await getGithubToken();
+  const octokit = new Octokit({ auth: token });
+
+  const { data } = await octokit.rest.repos.listForAuthenticatedUser({
+    sort: 'updated',
+    direction: 'desc',
+    visibility: 'all',
+    per_page: perPage,
+    page: page,
+  });
+
+  return data;
+};
